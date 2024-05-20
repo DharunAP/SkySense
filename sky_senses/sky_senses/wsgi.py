a2f224d08b56edb8836476ce51b4eaa4b0d45529
dharun <dharunpalanisamy5256@gmail.com>
2024-05-20 21:48:54 +0530

    final2

--- sky_senses/sky_senses/wsgi.py
+++ sky_senses/sky_senses/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sky_senses.settings')
 
 application = get_wsgi_application()
+
+app = application
