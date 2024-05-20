00aa0ac3e515d4741919a45f67e21bccf13b6746
Dharun AP <121315919+DharunAP@users.noreply.github.com>
2024-05-20 21:52:52 +0530

    Delete sky_senses directory

--- sky_senses/whatsapp_bot/management/commands/scheduler.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from whatsapp_bot.tasks import start_scheduler
-
-class Command(BaseCommand):
-    help = 'Starts the scheduler'
-
-    def handle(self, *args, **options):
-        start_scheduler()
